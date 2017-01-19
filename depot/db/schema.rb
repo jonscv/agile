4e1501a0c64b85eb1256ce79a15f7d9938149e01
users <businessitspecialist@googlemail.com>
2017-01-19 13:12:56 +0000

    Revert "delete"
    This reverts commit 0dc33535c823db96e1199b4209663267f3a5b672.

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118205626) do
+ActiveRecord::Schema.define(version: 20170118204547) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20170118205626) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "category"
-    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
