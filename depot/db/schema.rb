3dfe4985e30387b955004c2b02cab007d63dc6cb
users <businessitspecialist@googlemail.com>
2017-01-19 14:39:36 +0000

    db changes

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118204547) do
+ActiveRecord::Schema.define(version: 20170118205626) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170118204547) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "category"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
